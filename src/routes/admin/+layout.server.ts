4477401a7e7733e94086b514224b99e4eba3724b
Henrik VT <commits@henrikvt.com>
2025-01-29 18:20:07 -0500

    base admin layout

---
+++ src/routes/admin/+layout.server.ts
@@ -0,0 +1,15 @@
+import { redirect, type ServerLoadEvent } from '@sveltejs/kit';
+
+export const load = async (event: ServerLoadEvent) => {
+	if (!event.locals.user) {
+		return redirect(302, '/auth/login');
+	}
+
+	if (!event.locals.user.isAdmin) {
+		return redirect(302, '/home');
+	}
+
+	return {
+		user: event.locals.user
+	};
+};
