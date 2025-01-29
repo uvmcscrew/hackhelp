695dd0f035dfe10eb56f95f78c59412502d4d9d6
Henrik VT <commits@henrikvt.com>
2025-01-29 13:41:03 -0500

    move login route

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -4,10 +4,11 @@
 	import CardContent from '$lib/components/ui/card/card-content.svelte';
 	import CardHeader from '$lib/components/ui/card/card-header.svelte';
 	import CardTitle from '$lib/components/ui/card/card-title.svelte';
+	import { type GET as GetGithubAuthUrl } from './github/+server';
 </script>
 
 <CardHeader
 	><CardTitle>HackHelp Sign In</CardTitle><CardDescription>Sign in with GitHub</CardDescription
 	></CardHeader
 >
-<CardContent><Button>Sign In with Github</Button></CardContent>
+<CardContent><Button href="/auth/login/github">Sign In with Github</Button></CardContent>
