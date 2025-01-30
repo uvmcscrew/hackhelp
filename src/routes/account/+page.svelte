6d72668388eff24a423c8ef35467631b3f5259ec
Henrik VT <commits@henrikvt.com>
2025-01-30 16:42:36 -0500

    more useful account page

--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -9,19 +9,24 @@
 	import { Button } from '$lib/components/ui/button';
 	import { Badge } from '$lib/components/ui/badge';
 	import { goto } from '$app/navigation';
+	import CardContent from '@/components/ui/card/card-content.svelte';
 
 	let { data }: PageProps = $props();
 	const image = `https://avatars.githubusercontent.com/u/${data.user.githubId}`;
+
+	const inTeam = false;
 </script>
 
 <div class="mx-auto flex min-h-screen w-xl flex-col gap-y-4 pt-16">
 	<h1 class="w-full text-center text-2xl font-semibold">Account</h1>
 	<div class="text-foreground flex w-full justify-center">
-		<Button
-			variant="link"
-			class="hover:cursor-pointer"
-			href={data.user.isAdmin ? '/admin' : '/home'}><ArrowLeft class="h-8 w-8 " />Back</Button
-		>
+		{#if data.user.isInOrganization}
+			<Button
+				variant="link"
+				class="hover:cursor-pointer"
+				href={data.user.isAdmin ? '/admin' : '/home'}><ArrowLeft class="h-8 w-8 " />Back</Button
+			>
+		{/if}
 	</div>
 	<Card.Root
 		><Card.CardHeader><Card.CardTitle>Profile</Card.CardTitle></Card.CardHeader><Card.CardContent
@@ -34,7 +39,7 @@
 				<span class="inline-flex gap-x-2">
 					<h2 class="text-lg font-medium">{data.user.fullName}</h2>
 					{#if data.user.isAdmin}
-						<Badge class="rounded-full px-2 ">Admin</Badge>
+						<Badge class="rounded-full px-2" hoverEffects={false}>Admin</Badge>
 					{/if}
 				</span>
 				<span class="text-secondary-foreground font-mono">{data.user.username}</span>
@@ -43,12 +48,41 @@
 				<Button
 					variant="destructive"
 					title="Sign Out"
-					onclick={async (e) => {
+					onclick={async () => {
 						await fetch('/auth/logout', { method: 'POST' });
 						await goto('/auth/login');
 					}}><DoorOpen class="h-8 w-8" />Sign Out</Button
 				>
 			</div>
-		</Card.CardContent></Card.Root
-	>
+		</Card.CardContent>
+	</Card.Root>
+	<Card.Root>
+		<Card.Header><Card.Title>Participant Status</Card.Title></Card.Header>
+		<CardContent class="flex flex-col gap-y-2">
+			<div class="flex flex-row justify-between">
+				<span class="text-secondary-foreground font-semibold"
+					>Organization Membership Status:
+				</span>
+				{#if data.user.isInOrganization}
+					<Badge class="rounded-full bg-green-400 px-2" hoverEffects={false}>Joined</Badge>
+				{:else if data.user.isWhitelisted}
+					<Badge class="rounded-full bg-yellow-400 px-2 hover:cursor-pointer hover:bg-amber-600"
+						>Request Invitation</Badge
+					>
+				{:else}
+					<Badge class="rounded-full bg-red-400 px-2" hoverEffects={false}
+						>Contact Event Organizer</Badge
+					>
+				{/if}
+			</div>
+			<div class="flex flex-row justify-between">
+				<span class="text-secondary-foreground font-semibold">Team Membership Status: </span>
+				{#if data.user.teamId !== null}
+					<Badge class="rounded-full bg-green-600 px-2" hoverEffects={false}>Joined</Badge>
+				{:else}
+					<Badge class="rounded-full bg-yellow-600 px-2" hoverEffects={false}>Not in Team</Badge>
+				{/if}
+			</div>
+		</CardContent>
+	</Card.Root>
 </div>
