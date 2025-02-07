e5c6e61d33923910bfec0db2ae3a16813f49b171
Henrik VT <commits@henrikvt.com>
2025-02-07 12:02:43 -0500

    auto-select github api sdk using github org slug

--- src/lib/env/server.ts
+++ src/lib/env/server.ts
@@ -9,11 +9,9 @@ export const serverEnv = createEnv({
 	server: {
 		DATABASE_URL: z.string().nonempty(),
 		GITHUB_APP_ID: z.coerce.number().int().positive(),
-		GITHUB_APP_INSTALL_ID: z.coerce.number().int().positive(),
 		GITHUB_APP_PRIVATE_KEY: z.string().nonempty(),
 		GITHUB_APP_CLIENT_ID: z.string().nonempty(),
-		GITHUB_APP_CLIENT_SECRET: z.string().nonempty(),
-		CF_PAGES: z.coerce.boolean().optional().default(false)
+		GITHUB_APP_CLIENT_SECRET: z.string().nonempty()
 	},
 	client: {
 		PUBLIC_GITHUB_ORGNAME: z.string().nonempty()
--- src/lib/github.ts
+++ src/lib/github.ts
@@ -1,13 +1,23 @@
 import { App, Octokit } from 'octokit';
 import { serverEnv } from './env/server';
 
-export const githubApp = await new App({
+const github__ = new App({
 	appId: serverEnv.GITHUB_APP_ID,
 	privateKey: serverEnv.GITHUB_APP_PRIVATE_KEY
-}).getInstallationOctokit(serverEnv.GITHUB_APP_INSTALL_ID);
+});
+
+const [
+	{
+		installation: { id: appInstallId }
+	}
+] = (await Array.fromAsync(github__.eachInstallation.iterator())).filter(
+	(i) => i.installation.account?.login === serverEnv.PUBLIC_GITHUB_ORGNAME
+);
+
+export const githubApp = await github__.getInstallationOctokit(appInstallId);
 
 export const octokit = new Octokit({
 	request: {
-		fetch: fetch
+		fetch
 	}
 });
