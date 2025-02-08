40a2fdc2728517958a9a3dc2f292990f4a63f503
Henrik VT <commits@henrikvt.com>
2025-02-07 19:06:33 -0500

    winston logging

--- src/routes/auth/login/github/callback/+server.ts
+++ src/routes/auth/login/github/callback/+server.ts
@@ -7,12 +7,19 @@ import { db, schema } from '$lib/server/db';
 import { eq } from 'drizzle-orm';
 import { githubApp, octokit } from '$lib/github';
 import { serverEnv } from '$lib/env/server';
+import { logger } from '$lib/logger';
+
+const apiLogger = logger.child({ route: '/auth/login/github/callback', method: 'GET' });
 
 export async function GET(event: RequestEvent): Promise<Response> {
 	const code = event.url.searchParams.get('code');
 	const state = event.url.searchParams.get('state');
 	const storedState = event.cookies.get('github_oauth_state') ?? null;
+
+	apiLogger.info('Github OAuth callback', { code });
+
 	if (code === null || state === null || storedState === null) {
+		apiLogger.warn('Invalid state, 400 error', { error: 400, code });
 		return Response.json(
 			{
 				status: 400,
@@ -22,6 +29,7 @@ export async function GET(event: RequestEvent): Promise<Response> {
 		);
 	}
 	if (state !== storedState) {
+		apiLogger.warn('States do not match, 400 error', { error: 400, code });
 		return Response.json(
 			{
 				status: 400,
@@ -37,6 +45,7 @@ export async function GET(event: RequestEvent): Promise<Response> {
 	} catch (e: unknown) {
 		const err = e as Error;
 		// Invalid code or client credentials
+		apiLogger.warn('Could not validate OAuth Authorization Code', { error: 400, code });
 		return Response.json(
 			{
 				status: 400,
@@ -48,19 +57,51 @@ export async function GET(event: RequestEvent): Promise<Response> {
 	}
 
 	// --------- Get Github User Info ---------
-	const githubUserResponse = await octokit.rest.users.getAuthenticated({
-		headers: {
-			Authorization: `Bearer ${tokens.accessToken()}`
-		}
-	});
 
-	const userInOrg = (
-		await octokit.rest.orgs.listForAuthenticatedUser({
+	let githubUserResponse: { data: { login: string; name: string | null; id: number } };
+
+	try {
+		const res = await octokit.rest.users.getAuthenticated({
 			headers: {
 				Authorization: `Bearer ${tokens.accessToken()}`
 			}
-		})
-	).data.some((org) => org.login === serverEnv.PUBLIC_GITHUB_ORGNAME);
+		});
+		githubUserResponse = res;
+	} catch (e: unknown) {
+		const err = e as Error;
+		apiLogger.warn('Could not get authenticated user', { error: 400, code, ghError: err });
+		return Response.json(
+			{
+				status: 400,
+				message: 'Could not get authenticated user',
+				error: err.message
+			},
+			{ status: 400 }
+		);
+	}
+
+	let userInOrg: boolean;
+
+	try {
+		userInOrg = (
+			await octokit.rest.orgs.listForAuthenticatedUser({
+				headers: {
+					Authorization: `Bearer ${tokens.accessToken()}`
+				}
+			})
+		).data.some((org) => org.login === serverEnv.PUBLIC_GITHUB_ORGNAME);
+	} catch (e: unknown) {
+		const err = e as Error;
+		apiLogger.warn('Could not check if user is in organization', { error: 400, code, ghErr: err });
+		return Response.json(
+			{
+				status: 400,
+				message: 'Could not check if user is in organization',
+				error: err.message
+			},
+			{ status: 400 }
+		);
+	}
 
 	const userIsAdmin = userInOrg
 		? // This API call will error if the user is not in the specified organization
@@ -72,6 +113,8 @@ export async function GET(event: RequestEvent): Promise<Response> {
 			).data.role === 'admin'
 		: false;
 
+	apiLogger.info('USER FOUND', { user: githubUserResponse.data.login, admin: userIsAdmin });
+
 	// --------- Check Whitelist and Ban Status ---------
 	const [userStatus] = await db
 		.select({
@@ -82,9 +125,11 @@ export async function GET(event: RequestEvent): Promise<Response> {
 		.from(schema.userStatus)
 		.where(eq(schema.userStatus.username, githubUserResponse.data.login.toLowerCase()));
 
+	apiLogger.info('USER STATUS', { userStatus });
+
 	if (userStatus) {
-		console.log('userStatus', JSON.stringify(userStatus, null, 2));
 		if (userStatus.isBanned) {
+			apiLogger.warn('User is banned', { user: githubUserResponse.data.login });
 			return Response.json(
 				{
 					message: 'you are banned'
@@ -95,7 +140,9 @@ export async function GET(event: RequestEvent): Promise<Response> {
 			);
 		}
 	} else {
-		console.log('userStatus not found, creating');
+		apiLogger.info('User not found in userStatus, inserting', {
+			user: githubUserResponse.data.login
+		});
 		await db.insert(schema.userStatus).values({
 			username: githubUserResponse.data.login,
 			// Admins should be automatically whitelisted
@@ -109,18 +156,11 @@ export async function GET(event: RequestEvent): Promise<Response> {
 		.from(schema.user)
 		.where(eq(schema.user.githubId, githubUserResponse.data.id));
 
-	console.log(
-		'USER AUTH INFO',
-		JSON.stringify(
-			{
-				username: githubUserResponse.data.login,
-				exists: Boolean(existingUser),
-				knownStatus: userStatus ?? 'no'
-			},
-			null,
-			2
-		)
-	);
+	apiLogger.info('USER AUTH INFO', {
+		username: githubUserResponse.data.login,
+		exists: Boolean(existingUser),
+		knownStatus: userStatus ?? 'no'
+	});
 
 	if (existingUser) {
 		// Create and set session token
@@ -147,7 +187,10 @@ export async function GET(event: RequestEvent): Promise<Response> {
 			})
 			.where(eq(schema.userStatus.username, githubUserResponse.data.login));
 
-		console.log('known user logged in');
+		apiLogger.info('Known User Logged In', {
+			username: githubUserResponse.data.login,
+			admin: userIsAdmin
+		});
 
 		return new Response(null, {
 			status: 302,
@@ -169,7 +212,10 @@ export async function GET(event: RequestEvent): Promise<Response> {
 		})
 		.returning();
 
-	console.log('new user created');
+	apiLogger.info('New User Created', {
+		username: githubUserResponse.data.login,
+		admin: userIsAdmin
+	});
 
 	// Link userStatus to user
 	await db
