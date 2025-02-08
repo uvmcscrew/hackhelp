453589c8b03d20478909952c9373984a0627f50c
Henrik VT <commits@henrikvt.com>
2025-02-08 12:21:26 -0500

    set up tanstack query again

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,30 @@
 
 	let { children } = $props();
 	import { ModeWatcher } from 'mode-watcher';
+	import { browser } from '$app/environment';
+	import { QueryClient } from '@tanstack/svelte-query';
+	import { SvelteQueryDevtools } from '@tanstack/svelte-query-devtools';
+	import { PersistQueryClientProvider } from '@tanstack/svelte-query-persist-client';
+	import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
+
+	const queryClient = new QueryClient({
+		defaultOptions: {
+			queries: {
+				enabled: browser
+			}
+		}
+	});
+
+	const persister = createSyncStoragePersister({
+		storage: browser ? window.localStorage : null
+	});
 </script>
 
 <ModeWatcher />
 
-{@render children()}
+<PersistQueryClientProvider client={queryClient} persistOptions={{ persister }}>
+	<main>
+		{@render children()}
+	</main>
+	<SvelteQueryDevtools />
+</PersistQueryClientProvider>
