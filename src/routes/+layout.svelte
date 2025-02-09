84bfd1c66d53f8659766dd44aa71736f92ff51ef
Henrik VT <commits@henrikvt.com>
2025-02-09 17:29:42 -0500

    remove localstorage query data persister
    was not compatible with experimental svelte query

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,29 +4,26 @@
 	let { children } = $props();
 	import { ModeWatcher } from 'mode-watcher';
 	import { browser } from '$app/environment';
-	import { QueryClient } from '@tanstack/svelte-query';
+	import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
 	import { SvelteQueryDevtools } from '@tanstack/svelte-query-devtools';
-	import { PersistQueryClientProvider } from '@tanstack/svelte-query-persist-client';
-	import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
+	// import { PersistQueryClientProvider } from '@tanstack/svelte-query-persist-client';
+	// import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
 
 	const queryClient = new QueryClient({
 		defaultOptions: {
 			queries: {
-				enabled: browser
+				enabled: browser,
+				refetchInterval: 5000
 			}
 		}
-	});
-
-	const persister = createSyncStoragePersister({
-		storage: browser ? window.localStorage : null
 	});
 </script>
 
 <ModeWatcher />
 
-<PersistQueryClientProvider client={queryClient} persistOptions={{ persister }}>
+<QueryClientProvider client={queryClient}>
 	<main>
 		{@render children()}
 	</main>
 	<SvelteQueryDevtools />
-</PersistQueryClientProvider>
+</QueryClientProvider>
