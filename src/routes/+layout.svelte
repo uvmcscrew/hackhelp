8b39042fb1e3ca6c045037b8be703f5690cb3ac1
Henrik VT <commits@henrikvt.com>
2025-02-10 00:50:21 -0500

    auto-pop toast if query errors

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,17 +4,36 @@
 	let { children } = $props();
 	import { ModeWatcher } from 'mode-watcher';
 	import { browser } from '$app/environment';
-	import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
+	import {
+		MutationCache,
+		QueryCache,
+		QueryClient,
+		QueryClientProvider
+	} from '@tanstack/svelte-query';
 	import { SvelteQueryDevtools } from '@tanstack/svelte-query-devtools';
 	import { Toaster } from '$lib/components/ui/sonner';
 	import { PersistQueryClientProvider } from '@tanstack/svelte-query-persist-client';
 	import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
+	import { toast } from 'svelte-sonner';
+
+	const queryCache = new QueryCache({
+		onError: (error, query) => {
+			toast.error('Failed to fetch data', {
+				description: error.message
+			});
+		}
+	});
+
+	const mutationCache = new MutationCache();
 
 	const queryClient = new QueryClient({
+		queryCache,
+		mutationCache,
 		defaultOptions: {
 			queries: {
 				enabled: browser,
-				refetchInterval: 5000
+				refetchInterval: 90 * 1000, // 90 seconds at minimum
+				staleTime: 5 * 60 * 1000 // 5 minutes
 			}
 		}
 	});
