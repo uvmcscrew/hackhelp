ef8d139531b71cf84af6b148b75d4bb9702a7099
Henrik VT <commits@henrikvt.com>
2025-02-11 16:21:03 -0500

    account/auth/admin trpc router stuff

---
+++ src/lib/trpc/server/router/admin.ts
@@ -0,0 +1,40 @@
+import { adminProcedure, protectedProcedure, t } from '../shared';
+import { eq } from 'drizzle-orm';
+import { serverEnv } from '$lib/env/server';
+import type { Context } from '../context';
+import { TRPCError } from '@trpc/server';
+import type { db as dbClient, schema as dbSchema } from '$lib/server/db';
+import { z } from 'zod';
+
+// #############################################
+// #               USER ROUTER                 #
+// #############################################
+
+const allUserFilterSchema = z.object({
+	filter: z.string().optional()
+});
+
+const userRouter = t.router({
+	all: adminProcedure.query(async ({ ctx }) => {
+		const users = await ctx.db
+			.select()
+			.from(ctx.dbSchema.user)
+			.leftJoin(
+				ctx.dbSchema.userStatus,
+				eq(ctx.dbSchema.user.id, ctx.dbSchema.userStatus.linkedUserId)
+			);
+		return { users };
+	}),
+	allAccounts: adminProcedure.query(async ({ ctx }) => {
+		const users = await ctx.db.select().from(ctx.dbSchema.user);
+		return { users };
+	})
+});
+
+// #############################################
+// #               ADMIN ROUTER                #
+// #############################################
+
+export const adminRouter = t.router({
+	users: userRouter
+});
--- src/lib/trpc/server/router/index.ts
+++ src/lib/trpc/server/router/index.ts
@@ -1,12 +1,15 @@
 import { z } from 'zod';
 import { t } from '../shared';
-import { accountRouter } from './account';
+import { accountRouter, authRouter } from './account';
+import { adminRouter } from './admin';
 
 export const appRouter = t.router({
 	hello: t.procedure.input(z.string()).query((opts) => {
 		return { greeting: `Hello, ${opts.input}!` };
 	}),
-	account: accountRouter
+	account: accountRouter,
+	auth: authRouter,
+	admin: adminRouter
 });
 
 export { updateInvitedUser } from './account';
