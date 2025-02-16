646673f28650808676da6fe0aad7ea50ed4d5784
Henrik VT <commits@henrikvt.com>
2025-02-16 14:54:24 -0500

    competitor router stuff

--- src/lib/trpc/server/router/competitor.ts
+++ src/lib/trpc/server/router/competitor.ts
@@ -1,16 +1,15 @@
-import { adminProcedure, protectedProcedure, t } from '../shared';
+import { protectedProcedure, t } from '../shared';
 import { eq } from 'drizzle-orm';
 import { TRPCError } from '@trpc/server';
 import { z } from 'zod';
 import { serverEnv } from '$lib/env/server';
-import { team } from '$lib/server/db/schema';
 
 /**
  * This file contains most actions that a competitor will need. It is broken out into multiple routers depending on what the different actions
  * pertain to. These actions include; creating their team, updating it, creating tickets, updating tickets, handling repositories, etc.
  */
 
-const enforceUserIsInTeam = t.middleware(({ ctx, next }) => {
+const enforceUserIsInTeam = t.middleware(async ({ ctx, next }) => {
 	if (!ctx.user || !ctx.session) {
 		throw new TRPCError({ code: 'UNAUTHORIZED' });
 	}
@@ -19,10 +18,20 @@ const enforceUserIsInTeam = t.middleware(({ ctx, next }) => {
 		throw new TRPCError({ code: 'FORBIDDEN' });
 	}
 
+	const [team] = await ctx.db
+		.select()
+		.from(ctx.dbSchema.team)
+		.where(eq(ctx.dbSchema.team.id, ctx.user.teamId));
+
+	if (!team) {
+		throw new TRPCError({ code: 'NOT_FOUND', message: 'Team not found' });
+	}
+
 	return next({
 		ctx: {
-			user: ctx.user,
-			session: ctx.session
+			user: { ...ctx.user, teamId: ctx.user.teamId },
+			session: ctx.session,
+			team
 		}
 	});
 });
@@ -34,27 +43,36 @@ const teamProcedure = protectedProcedure.use(enforceUserIsInTeam);
 // #############################################
 
 const teamRouter = t.router({
-	get: protectedProcedure.query(async ({ ctx }) => {
-		const [team] = await ctx.db
-			.select()
-			.from(ctx.dbSchema.team)
-			.where(eq(ctx.dbSchema.team.id, ctx.user.teamId));
-		if (!team) {
-			throw new TRPCError({ code: 'NOT_FOUND', message: 'Team not found' });
-		}
-		return { team };
+	get: teamProcedure.query(async ({ ctx }) => {
+		const members = await ctx.db
+			.select({
+				id: ctx.dbSchema.user.id,
+				username: ctx.dbSchema.user.username,
+				fullName: ctx.dbSchema.user.fullName
+			})
+			.from(ctx.dbSchema.user)
+			.where(eq(ctx.dbSchema.user.teamId, ctx.team.id))
+			.leftJoin(ctx.dbSchema.profile, eq(ctx.dbSchema.user.id, ctx.dbSchema.profile.linkedUserId));
+		return { team: ctx.team, members };
 	}),
-	update: protectedProcedure
+	updateProperties: teamProcedure
 		.input(
 			z.object({
-				name: z.string().min(1),
-				canJoin: z.boolean()
+				name: z.string().nonempty(),
+				description: z.string().default('')
 			})
 		)
 		.mutation(async ({ ctx, input }) => {
+			const ghUpdate = await ctx.githubApp.rest.teams.updateInOrg({
+				org: serverEnv.PUBLIC_GITHUB_ORGNAME,
+				team_slug: ctx.team.githubSlug,
+				name: input.name,
+				description: input.description
+			});
+
 			const [team] = await ctx.db
 				.update(ctx.dbSchema.team)
-				.set({ name: input.name, canJoin: input.canJoin })
+				.set({ name: input.name, githubId: ghUpdate.data.id, githubSlug: ghUpdate.data.slug })
 				.where(eq(ctx.dbSchema.team.id, ctx.user.teamId))
 				.returning();
 			if (!team) {
