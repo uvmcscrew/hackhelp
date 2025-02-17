5d1e6abb84ad02f093d865261aa30e9e770abbed
Henrik VT <commits@henrikvt.com>
2025-02-16 23:03:54 -0500

    start making competitor home

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
+	import queries from '$lib/trpc/client/queries.svelte';
 	import type { PageProps } from './$types';
 
 	let { data }: PageProps = $props();
+
+	const account = queries.queryWhoami(data);
 </script>
 
-hello {data.user.username}
+<svelte:head>
+	<title>Competitor Home</title>
+</svelte:head>
+
+<div class="grid h-[calc(100vh-4rem-2rem)] w-screen grid-cols-3 grid-rows-2 p-2"></div>
