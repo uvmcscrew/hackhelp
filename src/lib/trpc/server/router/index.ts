9d49be5c15e9b4e9f9d247cabb21d96116a56a3f
Henrik VT <commits@henrikvt.com>
2025-02-16 19:09:45 -0500

    add competitor router to main appRouter

--- src/lib/trpc/server/router/index.ts
+++ src/lib/trpc/server/router/index.ts
@@ -1,15 +1,13 @@
-import { z } from 'zod';
 import { t } from '../shared';
 import { accountRouter, authRouter } from './account';
 import { adminRouter } from './admin';
+import { competitorRouter } from './competitor';
 
 export const appRouter = t.router({
-	hello: t.procedure.input(z.string()).query((opts) => {
-		return { greeting: `Hello, ${opts.input}!` };
-	}),
 	account: accountRouter,
 	auth: authRouter,
-	admin: adminRouter
+	admin: adminRouter,
+	competitor: competitorRouter
 });
 
 export { updateInvitedUser } from './account';
