8009fd15bd1a26d61f878edf836bb2eadddf1f34
Henrik VT <commits@henrikvt.com>
2025-02-19 16:24:44 -0500

    add team creation dialog box

--- src/routes/join-team/+page.server.ts
+++ src/routes/join-team/+page.server.ts
@@ -1,8 +1,36 @@
 import { createTeamSchema } from '$lib/schemas';
+import { fail, redirect } from '@sveltejs/kit';
+import type { PageServerLoad, Actions } from './$types.js';
 
 import { superValidate } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
+import { trpcCreateCaller } from '$lib/trpc/server/index.js';
+import { createCallerContext } from '$lib/trpc/server/context.js';
 
-export const load = async () => {
+export const load = (async () => {
 	return { createTeamForm: await superValidate(zod(createTeamSchema)) };
-};
+}) satisfies PageServerLoad;
+
+export const actions = {
+	default: async (event) => {
+		const form = await superValidate(event, zod(createTeamSchema));
+		if (!form.valid) {
+			return fail(400, {
+				createTeamForm: form
+			});
+		}
+
+		try {
+			await trpcCreateCaller(createCallerContext(event)).competitor.team.create(form.data);
+		} catch (e) {
+			const error = e as Error;
+
+			return fail(400, {
+				createTeamForm: form,
+				error: error.message
+			});
+		}
+
+		return redirect(302, '/home');
+	}
+} satisfies Actions;
--- src/routes/join-team/+page.svelte
+++ src/routes/join-team/+page.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
 	import * as Card from '$lib/components/ui/card';
-	import { FormButton } from '$lib/components/ui/form';
+	import * as Form from '$lib/components/ui/form';
 	import { superForm } from 'sveltekit-superforms';
 	import type { PageProps } from './$types';
+	import { Input } from '$lib/components/ui/input';
+	import { Textarea } from '$lib/components/ui/textarea';
 
 	let { data }: PageProps = $props();
+
+	const form = superForm(data.createTeamForm);
+	const { form: formData, enhance, submitting } = form;
 </script>
 
 <svelte:head>
@@ -16,6 +21,26 @@
 		<Card.Title>Create a team</Card.Title>
 	</Card.Header>
 	<Card.Content>
-		<form></form>
+		<form method="POST" use:enhance>
+			<Form.Field {form} name="name">
+				<Form.Control>
+					{#snippet children({ props })}
+						<Form.Label>Team Name</Form.Label>
+						<Input {...props} bind:value={$formData.name} />
+					{/snippet}
+				</Form.Control>
+				<Form.FieldErrors />
+			</Form.Field>
+			<Form.Field {form} name="description">
+				<Form.Control>
+					{#snippet children({ props })}
+						<Form.Label>Team Description</Form.Label>
+						<Textarea {...props} bind:value={$formData.description} />
+					{/snippet}
+				</Form.Control>
+				<Form.FieldErrors />
+			</Form.Field>
+			<Form.Button disabled={$submitting}>Create Team</Form.Button>
+		</form>
 	</Card.Content>
 </Card.Root>
