66a40d603f0e5762247120e7bf76b7a9ceac7efd
Henrik VT <commits@henrikvt.com>
2025-02-25 20:21:40 -0500

    fix invalid table html

--- src/routes/home/ticket-table-card.svelte
+++ src/routes/home/ticket-table-card.svelte
@@ -2,20 +2,23 @@
 	import Button from '$lib/components/ui/button/button.svelte';
 	import * as Card from '$lib/components/ui/card';
 	import * as Table from '$lib/components/ui/table';
+	import TicketCreateSheet from './ticket-create-sheet.svelte';
 </script>
 
 <Card.Card class="col-span-2 col-start-2 row-span-2 row-start-1">
 	<Card.Header class="flex flex-row items-center justify-between">
 		<Card.Title class="h-full">Tickets</Card.Title>
-		<Button size="default">Create Ticket</Button>
+		<TicketCreateSheet />
 	</Card.Header>
 	<Card.Content>
 		<Table.Root>
 			<Table.Header>
-				<Table.Head class="w-max">Title</Table.Head>
-				<Table.Head>Issue Link</Table.Head>
-				<Table.Head class="w-min">Status</Table.Head>
-				<Table.Head>Assigned To</Table.Head>
+				<Table.Row>
+					<Table.Head class="w-max">Title</Table.Head>
+					<Table.Head>Issue Link</Table.Head>
+					<Table.Head class="w-min">Status</Table.Head>
+					<Table.Head>Assigned To</Table.Head>
+				</Table.Row>
 			</Table.Header>
 			<Table.Body></Table.Body>
 		</Table.Root>
