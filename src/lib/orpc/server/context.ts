be4aecd4535a6adb36b6984f4e41e15d889053b3
Henrik VT <commits@henrikvt.com>
2026-01-03 17:07:34 -0500

    simplify context

--- src/lib/orpc/server/context.ts
+++ src/lib/orpc/server/context.ts
@@ -1,4 +1,3 @@
-import type { FetchCreateContextFnOptions } from '@trpc/server/adapters/fetch';
 import type { Session, User } from '$lib/server/db/schema';
 import { db, schema } from '$lib/server/db';
 import { githubApp } from '$lib/github';
@@ -11,21 +10,7 @@ export type ContextGeneratorParams = {
 	cookies: Cookies;
 };
 
-export type Context = Awaited<ReturnType<ReturnType<typeof createContextFunc>>>;
-
-export function createContextFunc(sveltekitCtx: ContextGeneratorParams) {
-	return async function ({ req }: FetchCreateContextFnOptions) {
-		return {
-			req,
-			...sveltekitCtx.locals,
-			db,
-			dbSchema: schema,
-			githubApp,
-			cookies: sveltekitCtx.cookies,
-			logger: logger.child({ requestId: req.headers.get('x-railway-request-id') })
-		};
-	};
-}
+export type Context = Awaited<ReturnType<typeof createOrpcContext>>;
 
 export function createOrpcContext(opts: ContextGeneratorParams) {
 	return {
@@ -37,16 +22,4 @@ export function createOrpcContext(opts: ContextGeneratorParams) {
 		cookies: opts.cookies,
 		logger: logger.child({ requestId: opts.request.headers.get('x-railway-request-id') })
 	};
-}
-
-export function createCallerContext(ctx: ContextGeneratorParams) {
-	return {
-		req: ctx.request,
-		...ctx.locals,
-		db,
-		dbSchema: schema,
-		githubApp,
-		cookies: ctx.cookies,
-		logger: logger.child({ requestId: ctx.request.headers.get('x-railway-request-id') })
-	} satisfies Context;
 }
