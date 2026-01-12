bebd42cecf1475f79b7738b19cfefdecefea99ad
Henrik VT <commits@henrikvt.com>
2026-01-12 18:37:31 -0500

    add flake dev shell

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "A Nix-flake-based Bun development environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-25.11";
+
+  outputs =
+    { self, ... }@inputs:
+
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import inputs.nixpkgs { inherit system; };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShellNoCC {
+            packages = with pkgs; [ bun ];
+          };
+        }
+      );
+    };
+}
