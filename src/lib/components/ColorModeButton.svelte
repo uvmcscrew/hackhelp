54a037c82e106b832a1815fb00a529051dcec28d
Henrik VT <commits@henrikvt.com>
2026-01-11 19:12:22 -0500

    fix mode-watcher svelte5 stuff

--- src/lib/components/ColorModeButton.svelte
+++ src/lib/components/ColorModeButton.svelte
@@ -5,9 +5,9 @@
 	import MoonIcon from 'lucide-svelte/icons/moon';
 </script>
 
-{#if $mode}
+{#if mode.current}
 	<Button onclick={toggleMode} variant="ghost" size="icon">
-		{#if $mode === 'dark'}
+		{#if mode.current === 'dark'}
 			<SunIcon class="h-8 w-8" />
 		{:else}
 			<MoonIcon class="h-8 w-8" />
