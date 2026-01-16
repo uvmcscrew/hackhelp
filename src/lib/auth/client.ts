3d72ab0807b84df75d380dbcfbfdfaefd3499587
Henrik VT <commits@henrikvt.com>
2026-01-16 14:33:18 -0500

    auth client plugins

--- src/lib/auth/client.ts
+++ src/lib/auth/client.ts
@@ -1,9 +1,21 @@
 import { createAuthClient } from 'better-auth/svelte';
 import { inferAdditionalFields } from 'better-auth/client/plugins';
 import type { auth } from './server';
+import { adminClient, emailOTPClient, magicLinkClient } from 'better-auth/client/plugins';
+import { passkeyClient } from '@better-auth/passkey/client';
+import { ac, roles } from './permissions';
 
 const authClient = createAuthClient({
-	plugins: [inferAdditionalFields<typeof auth>()]
+	plugins: [
+		inferAdditionalFields<typeof auth>(),
+		passkeyClient(),
+		emailOTPClient(),
+		magicLinkClient(),
+		adminClient({
+			ac,
+			roles
+		})
+	]
 });
 
 export const { signIn, signUp, useSession } = authClient;
