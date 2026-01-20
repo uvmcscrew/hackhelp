59a398b397774256dcbbedb7d2bb6694ffeb5bf2
Henrik VT <commits@henrikvt.com>
2026-01-19 19:30:40 -0500

    fix auth check in layout server load fn

--- src/routes/(auth)/+layout.server.ts
+++ src/routes/(auth)/+layout.server.ts
@@ -1,7 +1,7 @@
 import { redirect, type ServerLoadEvent } from '@sveltejs/kit';
 
 export const load = (event: ServerLoadEvent) => {
-	if (event.locals.user) {
+	if (event.locals.auth.user) {
 		return redirect(302, '/account');
 	}
 };
