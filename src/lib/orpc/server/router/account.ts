d63c7a58215232c4be3fe60b84e9931c8e66ad6a
Henrik VT <commits@henrikvt.com>
2026-01-30 15:24:59 -0500

    lots of procedures

--- src/lib/orpc/server/router/account.ts
+++ src/lib/orpc/server/router/account.ts
@@ -1,89 +1,82 @@
 import { o, protectedProcedure } from '../shared';
-import { eq } from 'drizzle-orm';
+import { and, eq } from 'drizzle-orm';
 import { serverEnv } from '$lib/env/server';
-import type { Context } from '../context';
-import type DB from '$lib/server/db';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { RequestError } from 'octokit';
-import { githubApp, octokit } from '$lib/github';
-import { generateState } from 'arctic';
-// import { githubOAuth } from '$lib/server/auth';
-import type { User } from '$lib/server/db/schema';
-import { ORPCError } from '@orpc/server';
-import { auth } from '$lib/auth/server';
+import type { AuthedContext } from '../context';
+import { octokit } from '$lib/github';
+import { ORPCError, type } from '@orpc/server';
 import { addRole, checkRolePermission } from '$lib/auth/permissions';
 
 // #############################################
 // #              ACCOUNT ROUTER               #
 // #############################################
 
-async function hasPendingInvite(ctx: Context & { user: { username: string } }) {
+async function hasPendingInvite(ctx: AuthedContext, githubUsername: string) {
 	const invites = await ctx.githubApp.rest.orgs.listPendingInvitations({
 		org: serverEnv.PUBLIC_GITHUB_ORGNAME
 	});
 
 	// If the username is present in the list of pending invites, return true
-	return invites.data.some((invite) => invite.login === ctx.user.username);
+	return invites.data.some((invite) => invite.login === githubUsername);
 }
 
-/**
- *
- * @param username the username to update
- * @param db an instance of teh database client
- * @param githubApp an instance of the github apps api client
- */
-export async function updateInvitedUser(
-	username: string,
-	db: typeof DB,
-	githubApp: typeof import('$lib/github').githubApp
-) {
-	let result: User;
+async function getProviderAccounts(context: AuthedContext, providerId: string) {
+	return await context.db.client
+		.select()
+		.from(context.db.schema.account)
+		.where(
+			and(
+				eq(context.db.schema.account.userId, context.user.id),
+				eq(context.db.schema.account.providerId, providerId)
+			)
+		);
+}
 
-	try {
-		const userIsMember = await githubApp.rest.orgs.getMembershipForUser({
-			username,
-			org: serverEnv.PUBLIC_GITHUB_ORGNAME
+async function getGithubUserInformation(context: AuthedContext) {
+	const accounts = await getProviderAccounts(context, 'github');
+
+	if (accounts.length === 0)
+		throw new ORPCError('BAD_REQUEST', {
+			message: 'You must have a linked GitHub account to make this request'
 		});
 
-		result = (
-			await db.client
-				.update(db.schema.user)
-				// If we get to this line, the user is a member of the organization. The api client will throw an error if they are not.
-				.set({ isOrgMember: true, isOrgAdmin: userIsMember.data.role === 'admin' })
-				.where(eq(db.schema.user.username, username))
-				.returning()
-		)[0];
-		// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	} catch (e) {
-		result = (
-			await db.client
-				.update(db.schema.user)
-				.set({ isOrgMember: false, isOrgAdmin: false })
-				.where(eq(db.schema.user.username, username))
-				.returning()
-		)[0];
-	}
+	const githubAccount = accounts[0];
 
-	return result;
+	try {
+		const [user, orgs] = (
+			await Promise.all([
+				octokit.rest.users.getAuthenticated({
+					headers: {
+						Authorization: `Bearer ${githubAccount.accessToken}`
+					}
+				}),
+				octokit.rest.orgs.listForAuthenticatedUser({
+					headers: {
+						Authorization: `Bearer ${githubAccount.accessToken}`
+					}
+				})
+			])
+		).map((p) => p.data);
+		return { user, orgs, providerAccount: githubAccount } as {
+			user: Awaited<ReturnType<typeof octokit.rest.users.getAuthenticated>>['data'];
+			orgs: Awaited<ReturnType<typeof octokit.rest.orgs.listForAuthenticatedUser>>['data'];
+			providerAccount: typeof githubAccount;
+		};
+	} catch (err) {
+		console.error(err);
+		throw new ORPCError('BAD_REQUEST', { message: 'Cannot authenticate to GitHub' });
+	}
 }
 
-export const accountRouter = {
-	whoami: protectedProcedure.handler(({ context }) => {
-		return { user: context.user };
-	}),
-	whoamiWithProfile: protectedProcedure.handler(async ({ context }) => {
-		// await updateInvitedUser(
-		// 	context.user.username,
-		// 	{ client: context.db.client, schema: context.db.schema },
-		// 	context.githubApp
-		// );
-		// const [userStatus] = await context.db.client
-		// 	.select()
-		// 	.from(context.db.schema.profile)
-		// 	.where(eq(context.db.schema.profile.linkedUserId, context.user.id));
-		return { user: context.user, session: context.session, userStatus };
-	}),
+type GetGithubProfile =
+	| { hasGithubProfile: false; message: string; orgStatus?: never; profile?: never }
+	| {
+			hasGithubProfile: true;
+			message?: never;
+			orgStatus: 'joined' | 'invited' | 'unknown';
+			profile: { username: string; fullName: string | null; avatar: string };
+	  };
 
+export const accountRouter = {
 	canCreateProfile: protectedProcedure.route({ method: 'GET' }).handler(({ context }) => {
 		return checkRolePermission({
 			roles: context.user.role || '',
@@ -127,131 +120,116 @@ export const accountRouter = {
 	}),
 
 	hasUvmProfile: protectedProcedure.handler(async ({ context }) => {
-		const accounts = await context.db.client
-			.select()
-			.from(context.db.schema.account)
-			.where(eq(context.db.schema.account.userId, context.user.id));
+		const accounts = await getProviderAccounts(context, 'uvm-netid');
 
-		for (const acc of accounts) {
-			if (acc.providerId === 'uvm-netid') return true;
-		}
+		if (accounts.length === 0) return false;
 
-		return false;
+		return true;
 	}),
-	hasPendingInvite: protectedProcedure.handler(async ({ context }) => {
-		const pendingInvite = await hasPendingInvite(context);
-		return { hasPendingInvite: pendingInvite };
-	}),
-	sendInviteMutation: protectedProcedure.route({ method: 'POST' }).handler(async ({ context }) => {
-		const rpclogger = context.logger.child({ procedure: 'account.sendInvite' });
-		// Make sure DB state is accurate
-		// const userstatus = await updateInvitedUser(
-		// 	context.user.username,
-		// 	{ client: context.db.client, schema: context.db.schema },
-		// 	context.githubApp
-		// );
-		// if (userstatus.isOrgMember) {
-		// 	rpclogger.info('User is already a member of the organization');
-		// 	throw new ORPCError('UNAUTHORIZED', {
-		// 		message: 'You are already a member of the organization'
-		// 	});
-		// }
 
-		// const pendingInvite = await hasPendingInvite(context);
-		// if (pendingInvite) {
-		// 	rpclogger.warn('User already has a pending invite');
-		// 	throw new ORPCError('UNAUTHORIZED', { message: 'You already have a pending invite' });
-		// }
-
-		// await context.githubApp.rest.orgs.createInvitation({
-		// 	org: serverEnv.PUBLIC_GITHUB_ORGNAME,
-		// 	invitee_id: context.user.githubId,
-		// 	role: 'direct_member'
-		// });
-
-		return { invited: true };
-	}),
-	refreshInviteMutation: protectedProcedure
-		.route({ method: 'POST' })
+	getGitHubProfile: protectedProcedure
+		.output(type<GetGithubProfile>((value) => value))
 		.handler(async ({ context }) => {
-			const rpclogger = context.logger.child({ procedure: 'account.refreshInvite' });
+			const { user, orgs } = await getGithubUserInformation(context);
 
-			// Make sure DB state is accurate
-			// const userstatus = await updateInvitedUser(
-			// 	context.user.username,
-			// 	{ client: context.db.client, schema: context.db.schema },
-			// 	context.githubApp
-			// );
-			// if (userstatus.isOrgMember) {
-			// 	rpclogger.info('User is already a member of the organization');
-			// 	return { refreshed: true, isMember: true };
-			// }
+			const orgStatus = orgs.some((org) => org.login === serverEnv.PUBLIC_GITHUB_ORGNAME)
+				? 'joined'
+				: (await hasPendingInvite(context, user.login))
+					? 'invited'
+					: 'unknown';
 
-			const pendingInvite = await hasPendingInvite(context);
-			if (!pendingInvite) {
-				rpclogger.warn('User does not have a pending invite');
-				return { refreshed: false, isMember: false };
-			}
+			return {
+				hasGithubProfile: true,
+				orgStatus,
+				profile: { username: user.login, fullName: user.name, avatar: user.avatar_url }
+			} satisfies GetGithubProfile;
+		}),
 
-			const orgmembers = await context.githubApp.rest.orgs.listMembers({
-				org: serverEnv.PUBLIC_GITHUB_ORGNAME
+	addGitHubUserToOrg: protectedProcedure.handler(async ({ context }) => {
+		const { user, orgs, providerAccount: githubAccount } = await getGithubUserInformation(context);
+
+		if (orgs.some((org) => org.login === serverEnv.PUBLIC_GITHUB_ORGNAME))
+			throw new ORPCError('UNAUTHORIZED', {
+				message: 'You are already a member of the organization'
 			});
 
-			const isMember = orgmembers.data.some((member) => member.login === context.user.username);
+		// If the user does not have an existing invite to the org, create one
+		if (!(await hasPendingInvite(context, user.login))) {
+			await context.githubApp.rest.orgs.createInvitation({
+				org: serverEnv.PUBLIC_GITHUB_ORGNAME,
+				invitee_id: user.id,
+				role: 'direct_member'
+			});
+		}
 
-			rpclogger.info('User org membership status', { isMember });
+		// Accept the pending invite
+		try {
+			const inviteResult = (
+				await octokit.rest.orgs.updateMembershipForAuthenticatedUser({
+					headers: {
+						Authorization: `Bearer ${githubAccount.accessToken}`
+					},
+					org: serverEnv.PUBLIC_GITHUB_ORGNAME,
+					state: 'active'
+				})
+			).data;
 
-			// if (isMember) {
-			// 	rpclogger.info('User is a member of the organization, updating user status');
-			// 	const result = await updateInvitedUser(
-			// 		context.user.username,
-			// 		{ client: context.db.client, schema: context.db.schema },
-			// 		context.githubApp
-			// 	);
-			// 	rpclogger.info('User status updated', {
-			// 		isOrgMember: result.isOrgMember,
-			// 		isOrgAdmin: result.isOrgAdmin
-			// 	});
-			// }
+			return inviteResult.state === 'active';
+		} catch (err) {
+			console.error(err);
+			throw new ORPCError('SERVER_ERROR', { message: 'Could not accept invite' });
+		}
+	}),
 
-			return { refreshed: true, isMember };
-		})
+	unlinkGitHubAccount: protectedProcedure.handler(async ({ context }) => {
+		const { user, orgs, providerAccount } = await getGithubUserInformation(context);
+
+		const orgStatus = orgs.some((org) => org.login === serverEnv.PUBLIC_GITHUB_ORGNAME)
+			? 'joined'
+			: (await hasPendingInvite(context, user.login))
+				? 'invited'
+				: 'unknown';
+
+		if (orgStatus === 'joined') {
+			// If the user is in the organization, remove them if they're not an owner
+			const userIsAdmin =
+				(
+					await context.githubApp.rest.orgs.getMembershipForUser({
+						org: serverEnv.PUBLIC_GITHUB_ORGNAME,
+						username: user.login
+					})
+				).data.role === 'admin';
+
+			if (!userIsAdmin) {
+				await context.githubApp.rest.orgs.removeMember({
+					org: serverEnv.PUBLIC_GITHUB_ORGNAME,
+					username: user.login
+				});
+			}
+		} else if (orgStatus === 'invited') {
+			// Cancel pending invitations
+			const invite = (
+				await context.githubApp.rest.orgs.listPendingInvitations({
+					org: serverEnv.PUBLIC_GITHUB_ORGNAME
+				})
+			).data.filter((i) => i.login === user.login)[0];
+
+			await context.githubApp.rest.orgs.cancelInvitation({
+				org: serverEnv.PUBLIC_GITHUB_ORGNAME,
+				invitation_id: invite.id
+			});
+		}
+
+		// Delete the provider account in the database
+		await context.db.client
+			.delete(context.db.schema.account)
+			.where(eq(context.db.schema.account.id, providerAccount.id));
+	})
 };
 
 // #############################################
 // #         AUTHENTICATION ROUTER             #
 // #############################################
-
-export async function authenticatedUserOrgStatus(username: string, accessToken: string) {
-	let userInOrg: boolean;
-
-	try {
-		userInOrg = (
-			await octokit.rest.orgs.listForAuthenticatedUser({
-				headers: {
-					Authorization: `Bearer ${accessToken}`
-				}
-			})
-		).data.some((org) => org.login === serverEnv.PUBLIC_GITHUB_ORGNAME);
-		// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	} catch (e) {
-		return { isInOrg: false, isAdmin: null };
-	}
-
-	if (userInOrg) {
-		const userIsAdmin =
-			(
-				await githubApp.rest.orgs.getMembershipForUser({
-					org: serverEnv.PUBLIC_GITHUB_ORGNAME,
-					username: username
-				})
-			).data.role === 'admin';
-
-		return { isInOrg: true, isAdmin: userIsAdmin };
-	}
-
-	return { isInOrg: false, isAdmin: null };
-}
 
 export const authRouter = {
 	getOAuthUrlMutation: o.route({ method: 'POST' }).handler(({ context: _ }) => {
