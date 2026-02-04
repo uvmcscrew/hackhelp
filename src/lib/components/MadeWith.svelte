0dc4041dc6d5add173fc73e9f42782d470020b40
Henrik VT <commits@henrikvt.com>
2026-02-04 16:20:00 -0500

    add little dev indicator

--- src/lib/components/MadeWith.svelte
+++ src/lib/components/MadeWith.svelte
@@ -1,11 +1,20 @@
 <script lang="ts">
 	import HeartIcon from 'lucide-svelte/icons/heart';
 	import SvelteLogo from './SvelteLogo.svelte';
+	import { dev } from '$app/environment';
 </script>
+
+{#snippet devModeIndicator()}
+	{#if dev}
+		<span class="px-1 font-semibold text-yellow-400">(dev mode)</span>
+	{/if}
+{/snippet}
 
 <span
 	class="text-muted-foreground inline-flex items-center justify-center text-center font-mono text-sm"
-	>HackHelp made with <HeartIcon class="h-6 w-6 fill-red-600 stroke-3 p-1 text-red-600" /> &
+	>HackHelp {@render devModeIndicator()} made with <HeartIcon
+		class="h-6 w-6 fill-red-600 stroke-3 p-1 text-red-600"
+	/> &
 	<SvelteLogo class="h-[1.7rem] w-[1.7rem] p-1" />
 	by
 	<a href="https://henrikvt.com" target="_blank" class="hover:text-accent-foreground px-1 underline"
