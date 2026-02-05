22ca6017044d615f3b80c7005ee9f902c385af9e
Henrik VT <commits@henrikvt.com>
2026-02-05 13:22:56 -0500

    cursed initialize

--- src/lib/github.ts
+++ src/lib/github.ts
@@ -1,22 +1,37 @@
 import { App, Octokit } from 'octokit';
 import { serverEnv } from './env/server';
+import { building } from '$app/environment';
 
 const github__ = new App({
 	appId: serverEnv.GITHUB_APP_ID,
 	privateKey: serverEnv.GITHUB_APP_PRIVATE_KEY
 });
 
-const [
-	{
-		installation: { id: appInstallId }
+export let githubApp: GithubAppClient;
+
+export type GithubAppClient = Awaited<ReturnType<typeof github__.getInstallationOctokit>>;
+
+void (async () => {
+	console.log('Github App ID: ', serverEnv.GITHUB_APP_ID);
+	console.log('Github Org Name: ', serverEnv.PUBLIC_GITHUB_ORGNAME);
+
+	if (building) {
+		console.log('Skipping Github App Client initialization during build');
+		return;
 	}
-] = (await Array.fromAsync(github__.eachInstallation.iterator())).filter(
-	(i) => i.installation.account?.login === serverEnv.PUBLIC_GITHUB_ORGNAME
-);
 
-export const githubApp = await github__.getInstallationOctokit(appInstallId);
+	const [
+		{
+			installation: { id: appInstallId }
+		}
+	] = (await Array.fromAsync(github__.eachInstallation.iterator())).filter(
+		(i) => i.installation.account?.login === serverEnv.PUBLIC_GITHUB_ORGNAME
+	);
 
-export type GithubAppClient = typeof githubApp;
+	console.log('Github App Install ID: ', appInstallId);
+
+	githubApp = await github__.getInstallationOctokit(appInstallId);
+})();
 
 export const octokit = new Octokit({
 	request: {
