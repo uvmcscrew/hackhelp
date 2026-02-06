2689da20a86a27967c6851bdca8df4794697bd93
Henrik VT <commits@henrikvt.com>
2026-02-05 22:17:58 -0500

    stuff

---
+++ src/routes/(authed)/admin/users/_components/user-table.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import * as Table from '$lib/components/ui/table';
+	type Props = {};
+
+	let {}: Props = $props();
+</script>
+
+<Table.Root>
+	<Table.Caption>Users</Table.Caption>
+	<Table.Header>
+		<Table.Row>
+			<Table.Cell>Name</Table.Cell>
+			<Table.Cell>Email</Table.Cell>
+			<Table.Cell>Role</Table.Cell>
+			<Table.Cell>Status</Table.Cell>
+		</Table.Row>
+	</Table.Header>
+</Table.Root>
