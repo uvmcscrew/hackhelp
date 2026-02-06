053e7a94325d83de6cbad65fa63e3bdd8ebae38a
Henrik VT <commits@henrikvt.com>
2026-02-05 23:18:30 -0500

    change query client initialization

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,5 +1,7 @@
 import posthog from 'posthog-js';
 import { browser, dev } from '$app/environment';
+import { MutationCache, QueryCache, QueryClient } from '@tanstack/svelte-query';
+import { toast } from 'svelte-sonner';
 
 export const load = () => {
 	if (browser && !dev) {
@@ -10,5 +12,38 @@ export const load = () => {
 			person_profiles: 'identified_only'
 		});
 	}
-	return;
+
+	const queryCache = new QueryCache({
+		onError: (error, _) => {
+			console.error(error);
+			toast.error('Failed to fetch data', {
+				description: error.message
+			});
+		}
+	});
+
+	const mutationCache = new MutationCache({
+		onError: (error) => {
+			console.error(error);
+			toast.error('Failed to perform action', {
+				description: error.message
+			});
+		}
+	});
+
+	const queryClient = new QueryClient({
+		queryCache,
+		mutationCache,
+		defaultOptions: {
+			queries: {
+				enabled: browser,
+				refetchInterval: 90 * 1000, // 90 seconds at minimum
+				staleTime: 5 * 60 * 1000, // 5 minutes
+				refetchOnWindowFocus: true,
+				refetchOnMount: 'always'
+			}
+		}
+	});
+
+	return { queryClient };
 };
