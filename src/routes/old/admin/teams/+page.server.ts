cb808f9e16bdd458ef729af029ef9f3deea105c8
Henrik VT <commits@henrikvt.com>
2026-02-24 18:42:53 -0500

    delete old stuff

--- src/routes/old/admin/teams/+page.server.ts
+++
@@ -1,7 +0,0 @@
-import { createOrpcContext } from '$lib/orpc/server/context';
-import { appRouter } from '$lib/orpc/server/router';
-import { type ServerLoadEvent } from '@sveltejs/kit';
-
-export const load = async (event: ServerLoadEvent) => {
-	return appRouter.admin.teams.getAll.callable({ context: createOrpcContext(event) })();
-};
